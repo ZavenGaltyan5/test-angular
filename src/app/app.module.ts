8ea6ac696a94f57274e270b75dd96940e01c6237
Z.Galstyan <ZavenGalstyan5>
2022-11-05 20:32:01 +0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,9 @@ import { HeaderComponent } from './header/header.component';
 import { SignUpTeacherComponent } from './sign-up/sign-up-teacher/sign-up-teacher.component';
 import { SignUpDonorComponent } from './sign-up/sign-up-donor/sign-up-donor.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
+import { VerifyAccComponent } from './sign-up/verify-acc/verify-acc.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
@@ -17,12 +21,16 @@ import {ReactiveFormsModule} from "@angular/forms";
     SignUpComponent,
     HeaderComponent,
     SignUpTeacherComponent,
-    SignUpDonorComponent
+    SignUpDonorComponent,
+    VerifyAccComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    MatSliderModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sign-up/verify-acc/verify-acc.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-acc',
+  templateUrl: './verify-acc.component.html',
+  styleUrls: ['./verify-acc.component.css']
+})
+export class VerifyAccComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
